54bf8d307dfe1cae1595e9a730961a880e1b3880
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-23 23:01:33 +0100

    Started project

---
+++ mailarchive.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       mailarchive
+Description   A mail archiver
+License       GPL version 2 (see GPL.txt for details)
+"""
+
+__author__ = "Enrico Bianchi"
+__copyright__ = "Copyright 2018, Enrico Bianchi"
+__credits__ = ["Enrico Bianchi", ]
+__license__ = "GPLv2"
+__maintainer__ = "Enrico Bianchi"
+__email__ = "enrico.bianchi@gmail.com"
+__status__ = "Development"
+__version__ = "0.0.0"
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
