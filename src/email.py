f66047428ca89864767becbee0c266b14bbaa634
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-23 23:51:07 +0100

    Added property

--- src/email.py
+++ src/email.py
@@ -7,5 +7,29 @@ License       GPL version 2 (see GPL.txt for details)
 
 
 class IMAP(object):
-    def __init__(self, parameter_list):
+    _host = None
+
+    def __init__(self):
         pass
+
+    @property
+    def host(self):
+        """
+        Hostname property
+        :return Return the hostname
+        """
+        return self._host
+
+    @host.getter
+    def host(self, host):
+        """
+        Set the hostname property
+        """
+        self._host = host
+
+    @host.deleter
+    def host(self):
+        """
+        Delete the hostname property
+        """
+        del self._host
