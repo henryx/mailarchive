f4c3a632a163c8939282d33f48fbe605fe243bdb
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-23 23:16:36 +0100

    Added modules

---
+++ src/email.py
@@ -0,0 +1,6 @@
+"""
+Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       mailarchive
+Description   A mail archiver
+License       GPL version 2 (see GPL.txt for details)
+"""
