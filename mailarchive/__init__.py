3505c90f95bbbd2dfe8fe0b2a0f26c143aabfba6
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-01-28 15:22:03 +0100

    Moved modules outside to the package

--- mailarchive/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
-Project       mailarchive
-Description   A mail archiver
-License       GPL version 2 (see GPL.txt for details)
-"""
-
-__all__ = ["database", "email"]
